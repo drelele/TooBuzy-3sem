9596530e61bbcc07e628740425bd1a1a7ab9c75e
Andre Devantier <andr254c@outlook.dk>
2019-04-03 12:37:27 +0200

    added service stack references

---
+++ Services/AppHost.cs
@@ -0,0 +1,21 @@
+﻿using Funq;
+using ServiceStack.WebHost.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("TooBuzy Services", typeof(AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            //Smid konfiguration ind, som ServiceStack skal bruge til containeren.
+        }
+    }
+}
